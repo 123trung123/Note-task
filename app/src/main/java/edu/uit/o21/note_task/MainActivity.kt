2bf431e6e900c9b097d37455f0623381b865e6ce
123trung123 <ngoctrungtran2904@gmail.com>
2024-06-08 20:42:41 +0700

    Commit 25 Updating color and layout for Notelist

--- app/src/main/java/edu/uit/o21/note_task/MainActivity.kt
+++ app/src/main/java/edu/uit/o21/note_task/MainActivity.kt
@@ -83,7 +83,7 @@ fun StyledButton(
             .width(180.dp)
             .padding(vertical = 10.dp),
         shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.buttonColors(Color.DarkGray)
+        colors = ButtonDefaults.buttonColors(Color(0xFF0277BD))
     ) {
         Text(text = text, color = Color.White)
     }
@@ -101,7 +101,7 @@ fun ContentButton(
             .height(50.dp)
             .padding(1.dp),
         shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.buttonColors(Color.DarkGray)
+        colors = ButtonDefaults.buttonColors(Color(0xFF0277BD))
     ) {
         Text(text = text, color = Color.White)
     }
@@ -116,7 +116,9 @@ fun HomeScreen(modifier: Modifier = Modifier,
            Row (modifier = Modifier
                .background(Color.White),
                verticalAlignment = Alignment.CenterVertically){
-                TopAppBar(modifier = Modifier.weight(1f).background(Color.Gray),
+                TopAppBar(modifier = Modifier
+                    .weight(1f)
+                    .background(Color.Gray),
                     title = { Text(text = "QUICK NOTE & TASK", fontStyle = FontStyle.Italic, fontFamily = FontFamily.Serif, fontWeight = FontWeight.ExtraBold) }
                 )
                  AsyncImage(
@@ -180,12 +182,13 @@ fun TheMain(
 ) {
     Column(
         modifier = Modifier
-            .background(Color.LightGray)
+            .background(Color(0xFFE3F2FD))
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        AsyncImage(
+        AsyncImage(modifier = Modifier
+            .fillMaxWidth(),
             model = "https://assets-global.website-files.com/5f7ece8a7da656e8a25402bc/631f32ee984371cb97df4ce2_How%20to%20take%20notes%20from%20a%20textbook-p-800.png",
             contentDescription = null,
         )
@@ -193,6 +196,7 @@ fun TheMain(
             StyledButton(onClick = toNoteDetail, modifier = Modifier,
                 text = "Note"
             )
+            Spacer(modifier = Modifier.padding(15.dp))
             StyledButton(onClick = toTaskDetail, modifier = Modifier,
                 text = "Task"
             )
@@ -211,7 +215,7 @@ fun NoteDetail(
     val state by noteViewModel.state.collectAsState()
     Column(
         modifier = Modifier
-            .background(Color.LightGray)
+            .background(Color(0xFFE3F2FD))
             .fillMaxSize()
             .padding(16.dp)
     ) {
@@ -247,7 +251,8 @@ fun NoteDetail(
         Spacer(modifier = Modifier.height(16.dp))
         Button(
             onClick = { noteViewModel.insertNote() },
-            modifier = Modifier.align(Alignment.End)
+            modifier = Modifier
+                .align(Alignment.End)
                 .width(130.dp)
                 .height(50.dp)
                 .padding(1.dp),
@@ -270,7 +275,7 @@ fun TaskDetail(
     val state by taskViewModel.state.collectAsState()
     Column(
         modifier = Modifier
-            .background(Color.LightGray)
+            .background(Color(0xFFE3F2FD))
             .fillMaxSize()
             .padding(16.dp)
     ) {
@@ -314,7 +319,8 @@ fun TaskDetail(
         Spacer(modifier = Modifier.height(16.dp))
         Button(
             onClick = { taskViewModel.insertTask() },
-            modifier = Modifier.align(Alignment.End)
+            modifier = Modifier
+                .align(Alignment.End)
                 .width(130.dp)
                 .height(50.dp)
                 .padding(1.dp),
@@ -356,14 +362,15 @@ fun NoteList(
 
     Column(
         modifier = Modifier
-            .background(Color.LightGray)
+            .background(Color(0xFFE3F2FD))
             .fillMaxSize()
-            .padding(16.dp)
+
     ) {
         Header(text = "NOTE LIST")
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(4.dp))
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             ContentButton(onClick = onClickBack, text = "Back")
@@ -393,7 +400,7 @@ fun NoteList(
                                 .padding(horizontal = 20.dp),
                             contentAlignment = Alignment.CenterStart
                         ) {
-                            Text("Deleting", color = Color.LightGray)
+                            Text("Deleting", color = Color(0xFFE3F2FD))
                         }
                     },
                     dismissContent = {
@@ -416,7 +423,13 @@ fun NoteList(
                                         onValueChange = { editingContent.value = it },
                                         modifier = Modifier.padding(10.dp)
                                     )
-                                    Button(onClick = {
+                                    Button(modifier = Modifier
+                                        .width(110.dp)
+                                        .height(40.dp)
+                                        .padding(1.dp),
+                                        shape = RoundedCornerShape(8.dp),
+                                        colors = ButtonDefaults.buttonColors(Color.Black),
+                                        onClick = {
                                         noteViewModel.updateNote(note.copy(title = editingTitle.value, content = editingContent.value))
                                         currentlyEditingNoteId.value = null
                                     }) {
@@ -433,7 +446,14 @@ fun NoteList(
                                         fontSize = 25.sp,
                                         modifier = Modifier.padding(10.dp)
                                     )
-                                    Button(onClick = {
+                                    Button(
+                                        modifier = Modifier
+                                            .width(110.dp)
+                                            .height(40.dp)
+                                            .padding(1.dp),
+                                        shape = RoundedCornerShape(8.dp),
+                                        colors = ButtonDefaults.buttonColors(Color.Black),
+                                        onClick = {
                                         currentlyEditingNoteId.value = note.id
                                         editingTitle.value = note.title
                                         editingContent.value = note.content
@@ -449,94 +469,6 @@ fun NoteList(
         }
     }
 }
-//
-//@Composable
-//fun NoteList(
-//    onClickBack: () -> Unit,
-//    toTheMain: () -> Unit,
-//    noteViewModel: NoteListViewModel = viewModel(factory = AppViewModelNt.Factory)
-//) {
-//    val state by noteViewModel.state.collectAsState()
-//    Column(
-//        modifier = Modifier
-//            .background(Color.LightGray)
-//            .fillMaxSize()
-//            .padding(16.dp)
-//    ) {
-//        Header(text = "NOTE LIST")
-//        Spacer(modifier = Modifier.height(4.dp))
-//        Row(
-//            modifier = Modifier.fillMaxWidth(),
-//            horizontalArrangement = Arrangement.SpaceBetween
-//        ) {
-//            ContentButton(onClick = onClickBack,
-//                text = "Back"
-//            )
-//            ContentButton(onClick = toTheMain,
-//                text = "Home"
-//            )
-//        }
-//        Spacer(modifier = Modifier.height(8.dp))
-//        LazyColumn {
-//            items(items = state.list_notes, key = { it.id }) { note ->
-//                val dismissState = rememberDismissState(
-//                    confirmValueChange = {
-//                        if (it == DismissValue.DismissedToEnd || it == DismissValue.DismissedToStart){
-//                            noteViewModel.deleteNoteById(note.id)
-//                        }
-//                        true
-//                    }
-//                )
-//                SwipeToDismiss(modifier=Modifier.border(1.dp, Color.Black),state = dismissState,
-//                    background = {
-//                    val color = when (dismissState.dismissDirection) {
-//                        DismissDirection.StartToEnd -> Color.Red
-//                        DismissDirection.EndToStart -> Color.Red
-//                        null -> Color.Transparent
-//                    }
-//                    Box(
-//                        modifier = Modifier
-//                            .fillMaxSize()
-//                            .background(color)
-//                            .padding(horizontal = 20.dp),
-//                        contentAlignment = Alignment.CenterStart
-//                    ) {
-//                        Text("Deleting", color = Color.LightGray)
-//                    }
-//                },
-//                    dismissContent = {
-//                Row(
-//                    modifier = Modifier
-//                        .fillMaxWidth(),
-//                    horizontalArrangement = Arrangement.SpaceBetween,
-//                    verticalAlignment = Alignment.CenterVertically
-//                ) {
-//                    Column {
-////                        Text(
-////                            text = note.id.toString(),
-////                            fontSize = 25.sp,
-////                            modifier = Modifier.padding(10.dp)
-////                        )
-//                        Text(
-//                            text = note.title,
-//                            fontSize = 25.sp,
-//                            fontWeight = FontWeight.Bold,
-//                            fontFamily = FontFamily.Serif,
-//                            modifier = Modifier.background(color = Color.Gray)
-//                                .fillMaxWidth()
-//                                .padding(5.dp)
-//                        )
-//                        Text(
-//                            text = note.content,
-//                            fontSize = 25.sp,
-//                            modifier = Modifier.padding(10.dp)
-//                        )
-//                    }
-//                }
-//            })}
-//        }
-//    }
-//}
 @Composable
 fun TaskList(
     onClickBack: () -> Unit,
@@ -549,7 +481,7 @@ fun TaskList(
 
     Column(
         modifier = Modifier
-            .background(Color.LightGray)
+            .background(Color(0xFFE3F2FD))
             .fillMaxSize()
             .padding(16.dp)
     ) {
@@ -634,7 +566,9 @@ fun TaskList(
                                 .border(1.dp, Color.Black)
                                 .fillMaxWidth()
                         ) {
-                            Row (verticalAlignment = Alignment.CenterVertically, modifier = Modifier.background(color = Color.Gray).fillMaxWidth()){
+                            Row (verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                                .background(color = Color.Gray)
+                                .fillMaxWidth()){
                                 Text(
                                     text = "Priority: ${task.priority}",
                                     fontWeight = FontWeight.Bold,
